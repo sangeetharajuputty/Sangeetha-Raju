3bec755d46de5ab557ba5bbfba3696a728eb183f
Raju <Sangeetha.Raju@Standardbank.co.za>
2017-05-09 13:38:41 +0200

    Design Completed

--- Bank/src/main/java/com/wipro/bank/assessment/App.java
+++ Bank/src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,47 @@
 package com.wipro.bank.assessment;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wipro.bank.entity.Account;
+
 public class App 
 {
-    public static void main( String[] args )
+
+    public static final List<Account> accounts=new ArrayList<Account>();
+    
+    public void Add(Account a)
     {
-        System.out.println( "Hello World!" );
+    	accounts.add(a);
     }
+    
+    public Account getAccountDetailsByID(int id)
+    {
+        int i;
+        Account result=null;
+        for(i=0;i<accounts.size();i++)
+        {
+        	if(accounts.get(i).getID()==id) result = accounts.get(i);
+        }
+		return result;
+    }
+    
+    public List<Account> getAccountDetailsByBalance(double amount)
+    {
+    	int i;
+    	List<Account> result=new ArrayList<Account>();
+        for(i=0;i<accounts.size();i++)
+        {
+        	if(accounts.get(i).getBalance()>=amount) result.add(accounts.get(i));
+        }
+        if(result.size()==0)
+        {
+        	return null;
+        }
+        else
+        {
+    		return result;
+        }
+    }
+    
 }
